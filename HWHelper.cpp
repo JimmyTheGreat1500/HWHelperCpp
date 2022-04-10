11f3fbc69365c6e7a720a4dc69306fa44ec80039
jimmythegreat1500 <asherclar08@gmail.com>
2022-04-10 10:47:48 -0400

    Initial commit

--- HWHelper.cpp
+++ HWHelper.cpp
@@ -1,25 +1,75 @@
-// HWHelper.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <Windows.h>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+//create a checkfolder function
+bool dirExists(const std::string& dirName_in)
+{
+    DWORD ftyp = GetFileAttributesA(dirName_in.c_str());
+    if (ftyp == INVALID_FILE_ATTRIBUTES)
+        return false;  //something is wrong with your path!
+
+    if (ftyp & FILE_ATTRIBUTE_DIRECTORY)
+        return true;   // this is a directory!
+
+    return false;    // this is not a directory!
+}
+
+//check if the terms of service have been accepted
+bool tosCheck() {
+    ifstream ifile;
+    ifile.open("./HWHelperFiles/TOS.hw");
+    if (ifile) {
+        return true;
+    }
+    ifile.open("./HWHelperFiles/TOS.hw");
+    if (ifile) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 int main()
 {
+	//set the console color to 0d and the title to "homework"
     system("Color 0d");
     SetConsoleTitleA("homework\n");
-    std::cout << "Hello World!\n";
+	
+	//make sure the additional files directory exists and if not, create it
+    bool dirStatus = dirExists("./HWHelperFiles");
+    if (dirStatus == false) {
+		system("mkdir ./HWHelperFiles");
+    }
+    
+    //call the tosCheck function, which tests if the terms of service have been accepted
+    bool tos = tosCheck();
+    if (tos == true) {
+        cout << "TOS  Already Accepted\nPlease wait while the program is loading...\n";
+    }
+    else {
+        //TOS has not been accepted, so we need to ask the user to accept it
+        system("cls");
+        cout << "\nTERMS OF SERVICE:\n---------------------------------------------------------------------\n- You will not reproduce this software.                             -\n- You will not claim ownership of this software.                    -\n- If you give us any feedback, we will not give credit to you.      -\n- You will not sell this software, for one can obtain it for free.  -\n---------------------------------------------------------------------/n";
+        cout << "\nDo you agree to the Terms of Service?\n1. I agree to all the terms specified above\n2. No, I'm really annoying so i wont LOL";
+        int tosChoice;
+		cin >> tosChoice;
+		if (tosChoice == 1) {
+           // tosYes();
+			}
+		else {
+            system("if exist TOS.hw del TOS.hw");
+            system("cls");
+			cout << "You have chosen not to accept the Terms of Service.\nPlease restart the program and accept the Terms of Service to continue.\n";
+			system("pause");
+            system("exit");
+	    }
+	}
+	
     system("pause");
 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
